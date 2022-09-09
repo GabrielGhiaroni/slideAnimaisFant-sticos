22298a922587c9fff91546d3c17fc0735625c6ca
Gabriel Ghiaroni <gabrielghiaroni@outlook.com>
2022-09-09 17:51:07 -0300

    caculos distancias quando os eventos ocorrem

--- js/slide.js
+++ js/slide.js
@@ -3,19 +3,37 @@ export default class Slide {
     constructor(slide, wrapper) {
         this.slide = document.querySelector(slide);
         this.wrapper = document.querySelector(wrapper);
+        this.distance = {
+            finalPosition: 0,
+            startX: 0,
+            movement: 0
+        };
     };
 
     onStart(event) {
         event.preventDefault();
         this.wrapper.addEventListener('mousemove', this.onMove);
+        this.distance.startX = event.clientX;
+    };
+
+    updatePosition(clientX) {
+        this.distance.movement = (this.distance.startX - clientX) * 1.6;
+        return this.distance.finalPosition - this.distance.movement;
     };
 
     onMove(event) {
+        const finalPosition = this.updatePosition(event.clientX);
+        this.moveSlide(finalPosition);
+    };
 
+    moveSlide(distance) {
+        this.distance.movePosition = distance;
+        this.slide.style.transform = `translate3d(${distance}px, 0, 0)`;
     };
 
     onEnd(event) {
         this.wrapper.removeEventListener('mousemove', this.onMove);
+        this.distance.finalPosition = this.distance.movePosition;
     };
 
     addSlideEvents() {
@@ -25,7 +43,7 @@ export default class Slide {
 
     bindEvents() {
         this.onStart = this.onStart.bind(this);
-        this.onMonve = this.onMove.bind(this);
+        this.onMove = this.onMove.bind(this);
         this.onEnd = this.onEnd.bind(this);
     };
 
